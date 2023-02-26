9aa560bee96d8464727062d85feb1b56ff01c5d5
mintolime <104442896+mintolime@users.noreply.github.com>
2023-02-26 13:46:21 +0000

    feat: added NotFound styles

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import './scss/app.scss';
-import Categories from './components/Categories';
 import React from 'react';
+
+import './scss/app.scss';
 import Header from './components/Header';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import PizzaSkeleton from './components/PizzaSkeleton';
+// import Home from './pages/Home'
+import NotFound from './pages/NotFoundPage/index';
 
 // import pizzas from './assets/pizzas.json';
 
 function App() {
-  const [pizzas, setpizzas] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://63fabf852027a45d8d5b2850.mockapi.io/items')
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setpizzas(res);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
-              : pizzas.map((data) => <PizzaBlock key={data.id} {...data} />)}
-          </div>
-        </div>
-      </div>
+      {/* <Home /> */}
+      <NotFound />
+      <div className="content"></div>
     </div>
   );
 }
---
+++ src/pages/NotFoundPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './NotFound.module.scss';
+console.log(styles);
+
+function NotFound() {
+  return (
+    <div className={styles.root}>
+      <h1 >Ничего не найдено 😔 </h1>
+      <p className={styles.decription}>К сожалению данная страница отсутствует...</p>
+    </div>
+  );
+}
+
+export default NotFound;
