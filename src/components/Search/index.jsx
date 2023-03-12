f30b02cc327c616142b7ffdcb9c07a4e8a7ebe1a
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-03-12 17:44:38 +0700

    feat: added icons clear and search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './Search.module.scss';
 
-function Search() {
-  return <input className={styles.root} placeholder="Поиск пиццы..." />;
+function Search({ searchValue, setSearchValue }) {
+  return (
+    <div className={styles.root}>
+    <img className={styles.search} src="img/search_icon.svg" alt="search logo" />
+      <img className={styles.clear} src="img/clear_icon.svg" alt="clear logo" onClick={()=>{setSearchValue('')}} />
+    <input
+    value={searchValue}
+      className={styles.input}
+      placeholder="Поиск пиццы..."
+      onChange={(evt) => setSearchValue(evt.target.value)}
+    />
+    </div>
+   
+  );
 }
 
 export default Search;
