23767ba3249e874c134d2281fd6b249915ec9cc7
mintolime <104442896+mintolime@users.noreply.github.com>
2023-08-01 10:31:36 +0000

    dev: func for Cart > added, delete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux';
 
 function Header({ searchValue, setSearchValue }) {
   const { items, totalPrice } = useSelector(state => state.cart)
-
+ 
+  const totalCount = items.reduce((sum,item)=> sum + item.count,0)
   return (
     <div className="header">
       <div className="container">
@@ -50,7 +51,7 @@ function Header({ searchValue, setSearchValue }) {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>{items.length}</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,12 +10,6 @@ const cardSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        // addItems(state, action) {
-        //     state.items.push(action.payload);
-        //     state.totalPrice = state.items.reduce((sum, obj) => {
-        //         return obj.price + sum
-        //     }, 0)
-        // },
         addItems(state, action) {
             const findItem = state.items.find(obj => obj.id === action.payload.id)
             
@@ -30,15 +24,28 @@ const cardSlice = createSlice({
                 return (obj.price * obj.count) + sum
             }, 0)
         },
+        // plusItem(state, action){
+        //     const findItem = state.items.find(obj => obj.id === action.payload)
+        //     if (findItem) {
+        //         findItem.count++
+        //     }
+        // },
+        minusItems(state, action){
+            const findItem = state.items.find(obj => obj.id === action.payload)
+            if (findItem) {
+                findItem.count--
+            }
+        },
         removeItems(state, action) {
             state.items = state.items.filter(obj => obj.id === !action.payload);
         },
         clearItems(state) {
             state.items = []
+            state.totalPrice = 0;
         },
     }
 });
 
-export const { addItems, removeItems, clearItems } = cardSlice.actions;
+export const { addItems, removeItems,minusItems, clearItems } = cardSlice.actions;
 
 export default cardSlice.reducer;
