c11297b05c75819b10358399d3476d3d8cec36be
mintolime <104442896+mintolime@users.noreply.github.com>
2023-08-27 11:12:46 +0000

    dev: install ts

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import { store } from './redux/store';
+import { Provider } from 'react-redux'
+
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+  );
+}
+
